61bc57d42f1e866fbdff04ec504aa5580440e274
DmitryZagorodnov <43351927+DmitryZagorodnov@users.noreply.github.com>
2022-10-24 00:24:49 +0300

    added first test (one equals one)

---
+++ src/tests/java/OneIsOneTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class OneIsOneTest {
+
+    @Test
+    void oneIsOne() {
+        assertEquals(1, 1);
+    }
+}
